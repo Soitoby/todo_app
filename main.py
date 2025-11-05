28135a6654144874ead9867869ae188c4d9c3c14
Ilia <soitoanime@gmail.com>
2025-11-05 05:20:53 +0300

    done main function

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def main():
 
         choice = questionary.select(
             "What do you want to do?",
-            choices=["create task", "watch task", "remove task","Close programm"],
+            choices=["create task", "watch task", "remove task","edit status","Close programm"]
         ).ask()
         try:
             match (choice):
                 case "create task":
-                    name_task = input("input name task:")
-                    discription = input("input discription:")
-                    task_lib.task(name_task,discription)
+                    task_lib.create_task()
                 case "watch task":
                     task_lib.watch_task()
                 case "remove task":
                     task_lib.remove_task()
                 case "Close programm":
                     run = False
+                case "edit status":
+                    task_lib.edit_status()
                 case _:
                     print("uncorrect input")
 
--- task_lib.py
+++ task_lib.py
@@ -1,74 +1,55 @@
 import os
-import termcolor, questionary 
+import termcolor, questionary
 
 
-class task():
-    def __init__(self, name,discription):
-        
-        self.name = name
-        self.path = f"task/{name}"
-        self.discription = discription
-        self.status = "create"
-        os.mkdir(self.path)
-
-        file_create = ["discription.txt","status.txt"]
-        file_write = [self.discription,self.status]
-        x = 0     
-        for i in file_create:  
-             
-            with open(f"{self.path}/{i}", "x") as f:
-                f.write(file_write[x])
-            x+=1
-
-
-
-
-        print(termcolor.colored(f"create {name}", "green"))
-        
-    def edit_status(self, status):
-        choice = questionary.select(
-            "choose status", choice=["select","progress","done"]
-        )   
-        match (choice):
-            case "select":
-                pass
-            case "progress":
-                pass
-            case "done":
-                pass
-
+def choose_task():
+    if os.path.isdir("task/"):
+        list = os.listdir("task/")
+    else:
+        return print(termcolor.colored("zero task", "red"))
+    
+    choice_file = questionary.select("choose task?", choices=list).ask()
+    return choice_file
 
 def create_task():
-    name = str(input("input name task:"))
-    discription = str(input("input discription:"))
-    status = "create"
-    with open(f"task/{name}.txt", "w") as f:
-        f.write(f"discription:{discription}\n")
-        f.write(f"status:{status}")
-    print("task create")
+    name_task = input("input name task:")
+    discription = input("input discription:")
+    path = f"task/{name_task}"
+    os.mkdir(path)
 
-def watch_task():
-    print()
-    for i in os.listdir("task/"):
-        print("\n",termcolor.colored(i,"red"))
-        with open(f"task/{i}","r") as f:
-            x = f.read().split()
-            for i in range(len(x)):
-                j = x[i].split(":")
-                print(f"{termcolor.colored(j[0],"blue")}:{j[1]}")
+    file_create = ["discription.txt", "status.txt"]
+    file_write = [discription, "create"]
 
-            
-    if input("return(Enter):") or True:
-        return 0
+    x = 0
+    for i in file_create:
+        with open(f"{path}/{i}", "x") as f:
+            f.write(file_write[x])
+        x += 1
 
-def remove_task():
-    list = os.listdir("task/")
-    y = 0
-    for i in list:
-        print(f"{y}. {i}")
-        y+=1
-    x = input("why task done:")
-    os.remove(f"task/{list[x]}")
+    print(termcolor.colored(f"create {name_task}", "green"))
+
 
 def edit_status():
-    pass
+    with open(f"task/{choose_task()}/status.txt","w") as f:
+        choice_status = questionary.select("choose status:", choices=["select","progress","done"]).ask()
+        f.write(choice_status)
+    
+    return print(termcolor.colored("edit status done", "green"))
+
+def remove_task():
+    task = choose_task()
+    for i in os.listdir(f"task/{task}"):
+        os.remove(f"task/{task}/{i}")
+    os.rmdir(f"task/{task}")
+    print(termcolor.colored("delete task","green"))
+
+def watch_task():
+    task = choose_task()
+    for i in os.listdir(f"task/{task}"):
+        with open(f"task/{task}/{i}","r") as f:
+            if i == "discription.txt":
+                print(termcolor.colored("discription: ","blue"), f.read())                
+            if i == "status.txt":
+                print(termcolor.colored("status: ","blue"), f.read())
+    if input("back(enter)") or True: return 0
+        
